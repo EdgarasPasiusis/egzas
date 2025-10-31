9df4f967d530d8f88d206881a8be8cf989b39bd8
EdgarasPasiusis <edgaras.pasiusis@stud.techin.lt>
2025-10-31 13:22:18 +0200

    final touches

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate, useLocation } from "react-router";
 import Nav from "./components/Nav";
 import LoginForm from "./components/pages/Loginform";
 import SignupForm from "./components/pages/SignupForm";
 import HomePage from "./components/pages/HomePage";
-import { UserContextProvider } from "./contexts/UserContext";
+import { useContext } from "react";
+import { UserContext } from "./contexts/UserContext";
 import BookDetail from "./components/pages/BookDetail";
 import ReservationsPage from "./components/pages/ReservationsPage";
 import AdminPanelPage from "./components/pages/AdminPanel";
@@ -13,26 +14,33 @@ import GenresManagmentPage from "./components/pages/GenresManagmentPage";
 import ReservationManagmentPage from "./components/pages/ReservationManagmentPage";
 
 function App() {
+  const { user } = useContext(UserContext);
+  const location = useLocation();
+
+  const publicPaths = ["/auth/login", "/auth/signup"];
+
+  if (!user && !publicPaths.includes(location.pathname)) {
+    return <Navigate to="/auth/login" replace />;
+  }
+
   return (
-    <UserContextProvider>
-      <div className="min-h-screen flex flex-col">
-        <Nav />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/auth/login" element={<LoginForm />} />
-            <Route path="/auth/signup" element={<SignupForm />} />
-            <Route path="/books/:id" element={<BookDetail />} />
-            <Route path="/reservations" element={<ReservationsPage />} />
-            <Route path="/admin" element={<AdminPanelPage />} />
-            <Route path="/admin/users" element={<UserManagmentPage />} />
-            <Route path="/admin/books" element={<BookManagmentPage />} />
-            <Route path="/admin/genres" element={<GenresManagmentPage />} />
-            <Route path="/admin/reservations" element={<ReservationManagmentPage />} />
-          </Routes>
-        </main>
-      </div>
-    </UserContextProvider>
+    <div className="min-h-screen flex flex-col">
+      <Nav />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth/login" element={<LoginForm />} />
+          <Route path="/auth/signup" element={<SignupForm />} />
+          <Route path="/books/:id" element={<BookDetail />} />
+          <Route path="/reservations" element={<ReservationsPage />} />
+          <Route path="/admin" element={<AdminPanelPage />} />
+          <Route path="/admin/users" element={<UserManagmentPage />} />
+          <Route path="/admin/books" element={<BookManagmentPage />} />
+          <Route path="/admin/genres" element={<GenresManagmentPage />} />
+          <Route path="/admin/reservations" element={<ReservationManagmentPage />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
